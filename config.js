91291bcf555dcebe96c50987e564dc57482b1d08
Xinyuu <56746258+Dzle@users.noreply.github.com>
2023-01-22 17:36:02 +0800

    更新数据

---
+++ config.js
@@ -0,0 +1,35 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: 'Public Status',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+	'm793482388-d14226ec06a5ffa11717ec1f',
+    'm793482391-1a51f85ff66a8373f4870434',
+
+  ],
+
+  // 日志天数
+  CountDays: 90,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: 'Homepage',
+      url: 'https://status.org.cn/'
+    },
+    {
+      text: 'GitHub',
+      url: 'https://github.com/yb/uptime-status'
+    },
+    {
+      text: 'Blog',
+      url: 'https://abo.xyz/'
+    },
+  ],
+};
