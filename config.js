4807a2cad67750cb6bb3498e1f120374d62c551f
Xinyuu <56746258+Dzle@users.noreply.github.com>
2023-02-20 21:31:04 +0800

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ window.Config = {
   ApiKeys: [
 	'm793482388-d14226ec06a5ffa11717ec1f',
 	'm793712533-a352e4b48f534156b8df0c3e',
-	'm793758852-e51b9f4843ca58ac3b3be0fb'
+	'm793758852-3c1d5616e0db069b3b84ee84'
 
   ],
 
